0b1e3f2410ce335d53d2c90a5cc3d85bb25093cd
Angel J <78835633+Iamanaws@users.noreply.github.com>
2024-12-28 11:46:29 -0800

    vm-vbox: disable efi

--- nixos/hosts/vm-vbox.nix
+++ nixos/hosts/vm-vbox.nix
@@ -8,10 +8,10 @@
     ++ lib.optional (systemType == "wayland") ../display/wayland.nix;
 
     boot.loader.grub.enable = true;
-    boot.loader.grub.device = "nodev";
+    boot.loader.grub.device = "/dev/sda";
     boot.loader.grub.useOSProber = true;
-    boot.loader.grub.efiSupport = true;
+    boot.loader.grub.efiSupport = false;
     boot.loader.systemd-boot.enable = lib.mkOverride 900 false;
-    boot.loader.efi.canTouchEfiVariables = lib.mkOverride 900 true;
+    boot.loader.efi.canTouchEfiVariables = lib.mkOverride 900 false;
   
 }
