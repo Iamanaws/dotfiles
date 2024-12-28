3c7a8b37e1eb03a20c664d6764563dddcffdce25
Angel J <78835633+Iamanaws@users.noreply.github.com>
2024-12-28 11:34:13 -0800

    vm-vbox: enable grub

--- nixos/hosts/vm-vbox.nix
+++ nixos/hosts/vm-vbox.nix
@@ -6,5 +6,11 @@
     [ ../hardware/vm-vbox.nix ./core.nix ] 
     ++ lib.optional (systemType == "x11") ../display/x11.nix
     ++ lib.optional (systemType == "wayland") ../display/wayland.nix;
+
+    boot.loader.grub.enable = true;
+    boot.loader.grub.device = "/dev/sda";
+    boot.loader.grub.useOSProber = true;
+    boot.loader.systemd-boot.enable = lib.mkOverride 900 false;
+    boot.loader.efi.canTouchEfiVariables = lib.mkOverride 900 false;
   
 }
