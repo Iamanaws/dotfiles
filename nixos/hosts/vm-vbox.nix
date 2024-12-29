5e1e7af0ce580b0d2d8a9d8e8fd90b0ad34c0be6
Angel J <78835633+Iamanaws@users.noreply.github.com>
2024-12-28 18:59:35 -0800

    vm-vbox: update

--- nixos/hosts/vm-vbox.nix
+++ nixos/hosts/vm-vbox.nix
@@ -5,13 +5,5 @@
   imports = 
     [ ../hardware/vm-vbox.nix ./core.nix ] 
     ++ lib.optional (systemType == "x11") ../display/x11.nix
-    ++ lib.optional (systemType == "wayland") ../display/wayland.nix;
-
-    boot.loader.grub.enable = true;
-    boot.loader.grub.device = "/dev/sda";
-    boot.loader.grub.useOSProber = true;
-    boot.loader.grub.efiSupport = false;
-    boot.loader.systemd-boot.enable = lib.mkOverride 900 false;
-    boot.loader.efi.canTouchEfiVariables = lib.mkOverride 900 false;
-  
+    ++ lib.optional (systemType == "wayland") ../display/wayland.nix;  
 }
