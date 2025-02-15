6efa34b58816766ab5349a07e174aafcada3fefb
iamanaws <78835633+Iamanaws@users.noreply.github.com>
2025-02-15 14:36:28 -0800

    dotfiles: remove unnecessary comments

--- nixos/roles/core/default.nix
+++ nixos/roles/core/default.nix
@@ -1,6 +1,3 @@
-# This is your system's configuration file.
-# Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
-
 { inputs, outputs, config, lib, pkgs, systemType, ... }:
 
 {
