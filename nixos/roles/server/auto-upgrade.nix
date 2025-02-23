6d51a5497e36db599d038690386de620faf9d3ea
iamanaws <78835633+Iamanaws@users.noreply.github.com>
2025-02-23 15:35:16 -0800

    nixos/roles/server: update config

--- nixos/roles/server/auto-upgrade.nix
+++ nixos/roles/server/auto-upgrade.nix
@@ -5,7 +5,7 @@
     allowReboot = lib.mkDefault true;
     dates = "02:00";
     flags = [ "--accept-flake-config" ];
-    flake = "github:iamanaws/dotfiles"; # config.environment.variables.FLAKE or
+    flake = "github:iamanaws/dotfiles";
     operation = "boot";
     persistent = true;
     randomizedDelaySec = "60min";
--- nixos/roles/server/default.nix
+++ nixos/roles/server/default.nix
@@ -9,14 +9,30 @@
 {
   imports = [ ../core ];
 
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [
+      80
+      443
+    ];
+    allowedUDPPorts = [ 53 ];
+  };
+
   services.openssh = {
     enable = true;
-    ports = [ 3232 ];
+    ports = [ 3939 ];
+    allowSFTP = false;
 
     settings = {
       PermitRootLogin = "no";
       PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      X11Forwarding = false;
+
+      "AllowTcpForwarding" = "yes";
+      "AllowAgentForwarding" = "no";
+      "AllowStreamLocalForwarding" = "no";
+      "AuthenticationMethods" = "publickey";
     };
   };
-
 }
