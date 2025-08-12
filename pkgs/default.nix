211404869ef4e2812d47c5a2ce8a150440a8ba61
iamanaws <78835633+Iamanaws@users.noreply.github.com>
2025-08-11 17:53:32 -0700

    pkgs: migrate to callPackage

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -4,69 +4,7 @@
 { pkgs }:
 
 {
-  kuyen-icons = import ./kuyen-icon-theme/package.nix {
-    inherit (pkgs)
-      lib
-      stdenvNoCC
-      fetchFromGitLab
-      hicolor-icon-theme
-      ;
-  };
-
-  antu-icons = import ./antu-icon-theme/package.nix {
-    inherit (pkgs)
-      lib
-      stdenvNoCC
-      fetchFromGitLab
-      hicolor-icon-theme
-      ;
-  };
-
-  dsnote = import ./dsnote/package.nix {
-    inherit (pkgs)
-      stdenv
-      lib
-      fetchFromGitHub
-      fetchzip
-      fetchurl
-      cmake
-      ninja
-      pkg-config
-      python3
-      fmt
-      openblas
-      mbrola
-      boost
-      stt
-      onnxruntime
-      pcaudiolib
-      spdlog
-      python312Packages
-      rhvoice
-      qt5
-      libsForQt5
-      opencl-headers
-      ocl-icd
-      amdvlk
-      cudaPackages
-      pcre2
-      extra-cmake-modules
-      libpulseaudio
-      xorg
-      autoconf
-      automake
-      libtool
-      which
-      libvorbis
-      ffmpeg
-      taglib
-      rubberband
-      libarchive
-      xz
-      lame
-      xdotool
-      wayland
-      wayland-protocols
-      ;
-  };
+  antu-icons = pkgs.callPackage ./antu-icon-theme/package.nix { };
+  dsnote = pkgs.callPackage ./dsnote/package.nix { };
+  kuyen-icons = pkgs.callPackage ./kuyen-icon-theme/package.nix { };
 }
